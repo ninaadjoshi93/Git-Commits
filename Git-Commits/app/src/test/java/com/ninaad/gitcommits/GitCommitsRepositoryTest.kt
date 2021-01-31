8d0096be161fef8fc2b8d287ddd7d02b41528dfc
ninaadjoshi <ninaadjoshi93@gmail.com>
2021-01-31 13:42:53 -0800

    Part-1 - added tests for repository methods

--- Git-Commits/app/src/test/java/com/ninaad/gitcommits/GitCommitsRepositoryTest.kt
+++ Git-Commits/app/src/test/java/com/ninaad/gitcommits/GitCommitsRepositoryTest.kt
@@ -1,20 +1,67 @@
 package com.ninaad.gitcommits
 
+import com.google.gson.Gson
+import com.ninaad.gitcommits.api.GitHubAPI
+import com.ninaad.gitcommits.model.GitResponseItem
 import com.ninaad.gitcommits.repository.GitCommitsRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
+import timber.log.Timber
+import java.io.IOException
+import java.nio.charset.Charset
 
 class GitCommitsRepositoryTest {
 
     lateinit var sut: GitCommitsRepository
+
+    @MockK
+    lateinit var mockApiClass: GitHubAPI
+
+    lateinit var output: List<GitResponseItem>
+
     @Before
     fun setUp() {
-//        val gitHubAPIMock =
-//        sut = GitCommitsRepository()
+        MockKAnnotations.init(this)
+        val inputStream = ClassLoader.getSystemClassLoader()
+            .getResourceAsStream("get_commits_success_response.json")
+        val reader = inputStream.bufferedReader(Charset.defaultCharset())
+        val content: String
+        try {
+            content = reader.readText()
+            output = Gson().fromJson(content, Array<GitResponseItem>::class.java).toList()
+            coEvery { mockApiClass.getCommits("kubernetes", "kubernetes") } returns output
+            coEvery { mockApiClass.getCommits("owner", "name") } returns emptyList()
+            sut = GitCommitsRepository(mockApiClass)
+        } catch (exception: IOException) {
+            Timber.i("Resource file not found")
+        } finally {
+            reader.close()
+        }
     }
 
-    fun testGetGitCommitsMethod() {
+    @Test
+    fun testGetGitCommitsEmptyMethod() = runBlocking {
+        val repoOwner = "owner"
+        val repoName = "name"
+        val expected = emptyList<GitResponseItem>()
+        val actual = sut.getGitCommits(repoOwner, repoName)
+        assertEquals(expected, actual)
+    }
 
+    @Test
+    fun testGetGitCommitsValidMethod() = runBlocking {
+        val repoOwner = "kubernetes"
+        val repoName = "kubernetes"
+        val expected = ArrayList(output)
+        val actual = sut.getGitCommits(repoOwner, repoName)
+        assertEquals(expected, actual)
+        assertEquals(expected.size, actual.size)
     }
 
     @After
